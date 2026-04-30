813416c8e33b45ec75f8d3d08ff578bd3f69003e
crbyy <ustyushenko1998@mail.ru>
2026-05-01 02:46:40 +0300

    1st_app

--- app.py
+++ app.py
@@ -8,6 +8,27 @@ app = Flask(__name__)
 def hello():
     return "Hello Flask!"
 
+@app.route('/hello')
+def hello_again():
+    return "Hello, world!"
+
+@app.route('/info')
+def info():
+    return f'This is an information page.'
+
+@app.route('/calc/<int:a>/<int:b>')
+def calc(a,b):
+    summ = a + b
+    return f'The sum of {a} and {b} is {summ}'
+
+@app.route('/reverse/<string>')
+def reverse(string):
+    return string[::-1]
+
+@app.route('/user/<name>/<int:age>')
+def user(name,age):
+    return f'Hello, {name}. You are {age} years old.'
+
 if __name__ == "__main__":
     app.run(debug=True)
 
