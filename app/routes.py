add8e61fe4d06aa2dfaafe5c676012f4d0d1a8d8
crbyy <ustyushenko1998@mail.ru>
2026-05-07 02:35:48 +0300

    app

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,4 @@
-from flask import Flask
-
-
-app = Flask(__name__)
-
+from app import app
 
 @app.route('/')
 def hello():
@@ -28,7 +24,3 @@ def reverse(string):
 @app.route('/user/<name>/<int:age>')
 def user(name,age):
     return f'Hello, {name}. You are {age} years old.'
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
