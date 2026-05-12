fc634a3e11c3bf280fef82a95fae2406b1517065
crbyy <ustyushenko1998@mail.ru>
2026-05-13 01:47:37 +0300

    7-10

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
 
 @app.route('/')
@@ -12,6 +12,29 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    if request.method == "POST":
+        name = request.form.get('name')
+        email = request.form.get('email')
+        color = request.form.get('color')
+        profession = request.form.get('profession')
+        hobbies = request.form.getlist('hobbies')
+        level =  request.form.get('level')
+        return render_template("result.html",
+                               name=name, email=email, color=color,
+                               profession=profession, hobbies=hobbies,level=level)
+    else:
+        return redirect(url_for("form"))
+
+
+
+
 
 
 
